c94fe81338bd486040413b7960a257506015182a
Elys <elys.sanntos@gmail.com>
2021-02-05 14:52:25 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.Forum/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,32 @@
+﻿using ByteBank.Forum.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        //Action para que o user se registre
+        public ActionResult Registrar()
+        {
+            //retornar a pagina que irei construir
+
+            return View();
+        }
+
+        //Para receber o post do user
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+
+
+            return View();
+
+        }
+
+
+    }
+}
---
+++ ByteBank.Forum/ByteBank.Forum/ViewModels/ContaRegistrarViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class ContaRegistrarViewModel
+    {
+        //Definir os atributos que o user vai preencher no formulario
+        //Esses atributos são obrigatorios, para indicar isso: inserir o atributo [Required]
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name ="Nome Completo")]
+        public string NomeCompleto { get; set; }
+        [Required] 
+        [EmailAddress]//Indica para o ASP .Net que este é um campo de E-mail
+        public string Email { get; set; }
+        [Required] 
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+    }
+}
